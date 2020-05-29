e3c8383d012bf3950d6d3c3ef5b53370715bf397
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-29 18:28:24 +0200

    fix

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
-const config = {
+// const config = {
+//   port: process.env.PORT || 3000,
+//   databaseUrl: 'mongodb+srv://dawidplesniarski:12345@cluster0-c4nxf.mongodb.net/test?retryWrites=true&w=majority',
+//   JwtSecret: process.env.JWT_SERCRET || 'secret',
+//
+// }
+// export default config
+export const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: 'mongodb+srv://dawidplesniarski:12345@cluster0-c4nxf.mongodb.net/test?retryWrites=true&w=majority',
-  JwtSecret: process.env.JWT_SERCRET || 'secret',
-
-}
-export default config
+  databaseUrl: process.env.MONGODB_URI || 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
+  JwtSecret: 'ajuwhqywekqwe12'
+};
