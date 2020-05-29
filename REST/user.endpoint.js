cac884e86c3d49b800eb984f2bd209d3449f11da
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-29 18:58:43 +0200

    fix

--- REST/user.endpoint.js
+++ REST/user.endpoint.js
@@ -2,15 +2,16 @@
 
 import business from '../business/business.container';
 import applicationException from '../service/applicationException';
+// import auth from '../middleware/auth';
 const admin = require('../middleware/admin');
 
-
-
 import auth from '../middleware/auth';
-const userEndpoint = (router) => {
+const userEndpoint = router => {
   router.post('/api/user/auth', async (request, response, next) => {
     try {
-      let result = await business(request).getUserManager(request).authenticate(request.body.login, request.body.password);
+      let result = await business(request)
+        .getUserManager(request)
+        .authenticate(request.body.login, request.body.password);
       response.status(200).send(result);
     } catch (error) {
       applicationException.errorHandler(error, response);
