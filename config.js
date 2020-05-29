f580572eef340f38db3c0298fa9d7a36dff3c702
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-29 19:14:43 +0200

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
+  // databaseUrl: 'mongodb+srv://dawidplesniarski:12345@cluster0-c4nxf.mongodb.net/test?retryWrites=true&w=majority',
   port: process.env.PORT || 3000,
-  databaseUrl: 'mongodb+srv://dawidplesniarski:12345@cluster0-c4nxf.mongodb.net/test?retryWrites=true&w=majority',
+  databaseUrl: process.env.MONGODB_URI || 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
   JwtSecret: 'ajuwhqywekqwe12'
 }
 export default config
-
